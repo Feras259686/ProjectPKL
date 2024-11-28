cf45e9a083e0c8e624a761680578c78ba47a18b2
{Feras259686} <{p22-alphariza493@smkn7-smr-sch.id}>
2024-11-28 11:01:03 +0800

    first commit

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+<!-- buat koneksi php -->
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "oim";
+
+$koneksi = mysqli_connect($host, $username, $password, $database);
+if (mysqli_connect_errno()) {
+   die("" . mysqli_connect_error());
+}
+?>

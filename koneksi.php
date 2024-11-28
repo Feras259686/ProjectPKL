c87e4655d2cff4409a0b86fc3f5ebf745b0023ec
Feras259686 <p22-alphariza493@smkn7-smr-sch.id>
2024-11-28 15:08:57 +0800

    setup database

--- koneksi.php
+++ koneksi.php
@@ -7,5 +7,7 @@ $database = "oim";
 $koneksi = mysqli_connect($host, $username, $password, $database);
 if (mysqli_connect_errno()) {
    die("" . mysqli_connect_error());
+} else {
+   echo ("Koneksi Berhasil");
 }
 ?>
